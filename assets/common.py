3babfd67ef7a289680fa0d6f1e9eca69cb3065f0
Matthew Dillon <matthewrdillon@gmail.com>
2020-12-04 07:27:19 -0700

    maint: set q2d2 UA

--- assets/common.py
+++ assets/common.py
@@ -133,8 +133,8 @@ class AnacondaConnection:
 
     def download(self, path, filehandle):
         url = os.path.join(self.URI, self._channel, path)
-        print(url, file=sys.stderr)
-        fh = urllib.request.urlopen(url)
+        req = urllib.request.Request(url, headers={'User-Agent': 'q2d2/2.0'})
+        fh = urllib.request.urlopen(req)
         shutil.copyfileobj(fh, filehandle)
 
     def upload_local_data(self, data, name, version):
